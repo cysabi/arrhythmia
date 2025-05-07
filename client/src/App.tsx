41a00832c7985109ef2cff68da189d706960f72e
cysabi <47790183+cysabi@users.noreply.github.com>
2025-05-07 16:48:18 -0400

    wahoo types
    Co-authored-by: Shaylee Xie <shailstorm@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,32 @@
+import type { TurnPayload } from "./types";
+
+export const sampleTurnList: TurnPayload[] = [
+  {
+    playerId: "1",
+    turnCount: 0,
+    action: "moveLeft",
+    checksum: "abc123",
+  },
+  {
+    playerId: "2",
+    turnCount: 0,
+    action: "shootFire",
+    checksum: "abc123",
+  },
+  {
+    playerId: "1",
+    turnCount: 1,
+    action: "skip",
+    checksum: "abc1234",
+  },
+  {
+    playerId: "2",
+    turnCount: 1,
+    action: "moveDown",
+    checksum: "abc1234",
+  },
+];
+
 function App() {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
--- server/main.go
+++ server/main.go
@@ -6,22 +6,54 @@ import (
 	"github.com/olahol/melody"
 )
 
+type Action string
+
+const (
+	MoveUp    Action = "moveUp"
+	MoveDown         = "moveDown"
+	MoveLeft         = "moveLeft"
+	MoveRight        = "moveRight"
+	Skip             = "skip"
+	Shoot            = "shoot"
+)
+
+type TurnPayload struct {
+	playerId  string
+	turnCount int
+	action    string
+	checksum  string
+}
+
+type GameState struct {
+	turnHistory []TurnPayload
+	players     []Player
+	checksum    string
+}
+
+type Player struct {
+	id            string
+	lastValidTurn int
+}
+
 func main() {
 	m := melody.New()
-
-	http.Handle("/", http.FileServer(http.Dir("../client/dist")))
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		m.HandleRequest(w, r)
 	})
 
+	// listenFor MovePayload
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		m.Broadcast(msg)
+		turn := TurnPayload{} // msg
+
+		// ~ if higher move index
+		// ~ ~ generate new checksum
+
+		// ~ compare checksum
+		// ~ ~ if invalid checksum : broadcast CatchupPayload
+		// ~ ~ else : broadcast move to other player
+
 	})
 
 	http.ListenAndServe(":5000", nil)
-
-	// i will be in person tomorrow YAY OK . MORNING. BEFORE 1??
-	// ok !!
-	// lets pair :D when ?SORRY U CAN GO I CAN TXT  OKOK BYEEE HAVE FUN
 }
