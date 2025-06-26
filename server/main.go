212c7fc1652c8a5214e5aab54da98ce75423e551
Jack Ratner <jackratner@gmail.com>
2025-06-26 15:01:09 -0400

    lobbies backend

--- server/main.go
+++ server/main.go
@@ -18,9 +18,86 @@ type Lobby struct {
 	id string
 	player_ids []string
 }
-
 type WaitingRoom struct {
-	lobbies []Lobby
+	lobbies	map[string]Lobby
+}
+
+func (w WaitingRoom) CleanLobbies() {
+	for k,v := range w.lobbies {
+		if len(v.player_ids) == 0 {
+			delete(w.lobbies, k)
+		}
+	}				
+
+	lobby_id := w.NextLobbyId()
+
+	w.lobbies[lobby_id] = Lobby {
+		id: lobby_id,
+		player_ids: []string{},
+	}
+}
+
+func (w WaitingRoom) NextLobbyId() string {
+	for _, c := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+	  _, has_key := w.lobbies[string(c)]
+		if !has_key {
+		  return string(c)
+		}
+	}
+
+	// TODO: better error handling
+	return ""
+}
+
+func (w WaitingRoom) AssignPlayer(lobby_id string, pid PlayerId) {
+  lobby := w.lobbies[lobby_id]
+	lobby.player_ids = append(lobby.player_ids, string(pid))
+	w.lobbies[lobby_id] = lobby
+	w.CleanLobbies()
+}
+
+func (w WaitingRoom) LobbyForPlayer(pid PlayerId) Lobby {
+	for _, l := range w.lobbies {
+		for _, s := range l.player_ids {
+				if s == string(pid) {
+  				return l
+				}
+		}
+	}
+
+  // TODO: better error case
+	return Lobby {}
+}
+
+func (w WaitingRoom) Start(pid PlayerId) {
+	lobby := w.LobbyForPlayer(pid)
+	delete(w.lobbies, lobby.id)
+}
+
+func (w WaitingRoom) LobbiesPayload() PayloadLobbies {
+  lobbies := []Lobby{}
+	for _, v := range w.lobbies {
+		lobbies = append(lobbies, v)
+	}
+
+	return PayloadLobbies {
+		lobbies: lobbies,
+	}
+}
+
+func (w WaitingRoom) New() WaitingRoom {
+	w = WaitingRoom {
+		lobbies: make(map[string]Lobby),
+	}
+
+	lobby_id := "A"
+
+	w.lobbies[lobby_id] = Lobby {
+		id: lobby_id,
+		player_ids: []string{},
+	}
+
+	return w
 }
 
 func (g Game) New() Game {
@@ -57,6 +134,7 @@ func (g *Game) BroadcastMissing(m *melody.Melody) {
 func main() {
 	m := melody.New()
 	game := Game{}.New()
+	waiting_room := WaitingRoom{}.New()
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		m.HandleRequest(w, r)
@@ -73,7 +151,7 @@ func main() {
 		pid := PidGenerate()
 		s.Set("pid", pid)
 
-		// TODO: Send lobbies
+		s.Write([]byte(waiting_room.LobbiesPayload().Send()))
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
@@ -87,11 +165,12 @@ func main() {
 		// 	if player turn is on game turn, set actedThisBeat and broadcast move to other players
 		//  broadcast move to other players so they're at most 1 turn behind
 
-		key, _ := s.Get("pid")
+		key := s.MustGet("pid")
 		pid := key.(PlayerId)
 		switch payload := receivePayload(pid, string(msg)).(type) {
 
 		case PayloadAction:
+			// TODO: CY - only send to members of lobby
 			for payload.turnCount > game.turnCount {
 				game.BroadcastMissing(m)
 				game.actedThisBeat = make(map[PlayerId]bool)
@@ -103,6 +182,9 @@ func main() {
 			}
 
 		case PayloadStart:
+			// TODO: CY - only send to members of lobby
+
+			waiting_room.Start(pid);
 			sessions, _ := m.Sessions()
 
 			// reset state
@@ -126,11 +208,12 @@ func main() {
 				payload.you = pids[i]
 				s.Write([]byte(payload.Send()))
 			}
-		}
 
 		case PayloadJoin:
-					// TODO: assign pid to lobby
-					// broadcast lobbies
+			// TODO: CY - send this from the client!
+			waiting_room.AssignPlayer(payload.lobby_id, payload.player_id)
+			m.Broadcast([]byte(waiting_room.LobbiesPayload().Send()))
+		}
 	})
 
 	const PORT = ":5199"
